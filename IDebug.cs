9556e9f60a6e31fad265b8846f99ff80eeb642c9
irshad khan <irshadpathan@users.noreply.github.com>
2017-12-07 16:34:28 +0530

    Add files via upload

---
+++ IDebug.cs
@@ -0,0 +1,57 @@
+﻿// *********** Developer Irshad Khan : http://www.irshadkhan.info ************** Open source license **************//
+
+
+
+/// <summary>
+/// Debug massage while development a game
+/// </summary>
+public sealed class IDebug
+{
+	
+	/// <summary>
+	/// Log the specified massage.
+	/// </summary>
+	/// <param name="massage">Massage.</param>
+	public static  void Log(string massage){UnityEngine.Debug.Log("<color=#00ff00ff>"+"<b>"+massage+"</b>"+"</color>");}
+
+
+	/// <summary>
+	/// Log the specified massage and Break.
+	/// </summary>
+	/// <param name="massage">Massage.</param>
+	/// <param name="Break">If set to <c>true</c> break.</param>
+	public static  void Log(string massage,bool Break){UnityEngine.Debug.Log ("<color=#00ff00ff>"+"<b>" + massage +"</b>"+ "</color>");if (Break)
+				UnityEngine.Debug.Break ();}
+
+
+	/// <summary>
+	/// Logs the error.
+	/// </summary>
+	/// <param name="massage">Massage.</param>
+	public static  void LogError(string massage){UnityEngine.Debug.Log("<color=#ff0000ff>"+"<b>"+massage+"</b>"+"</color>");}
+
+	/// <summary>
+	/// Logs the error.
+	/// </summary>
+	/// <param name="massage">Massage.</param>
+	/// <param name="Break">If set to <c>true</c> break.</param>
+	public static  void LogError(string massage,bool Break){UnityEngine.Debug.Log ("<color=#ff0000ff>"+"<b>" + massage +"</b>" +"</color>");if (Break)
+			UnityEngine.Debug.Break ();}
+
+	/// <summary>
+	/// Logs the warning.
+	/// </summary>
+	/// <param name="massage">Massage.</param>
+	public static void LogWarning(string massage){UnityEngine.Debug.Log("<color=#ffff00ff>"+"<b>"+massage+"</b>"+"</color>");}
+
+
+	/// <summary>
+	/// Logs the warning.
+	/// </summary>
+	/// <param name="massage">Massage.</param>
+	/// <param name="Break">If set to <c>true</c> break.</param>
+	public static  void LogWarning(string massage,bool Break){UnityEngine.Debug.Log ("<color=#ffff00ff>"+"<b>" + massage +"</b>" +"</color>");if (Break)
+			UnityEngine.Debug.Break ();}
+
+}
+
